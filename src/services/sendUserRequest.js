0ade64102a473802aca017af32c27a05029634b8
Sergei Palmer <zigpalmer@gmail.com>
2021-12-02 18:37:14 +0000

    created a user request function

--- src/services/sendUserRequest.js
+++ src/services/sendUserRequest.js
@@ -1,11 +1,16 @@
 const sendUserRequest = (props) => {
   const mins = props.mins
   const location = props.text
-  
-  return (
-    <>
-    </>
-  )
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ mins: {mins}, location: {location} })
+  };
+
+  fetch('postURL', requestOptions)
+    .then(response => response.json())
+    .then(data => console.log(data));
 }
 
 export default sendUserRequest
