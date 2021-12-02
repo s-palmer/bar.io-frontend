c4ba8b6131514cd7d1a7917cae1719487a2bf963
Sergei Palmer <zigpalmer@gmail.com>
2021-12-02 17:56:02 +0000

    Added slider to userinput form

--- src/components/UserInputForm.js
+++ src/components/UserInputForm.js
@@ -1,33 +1,63 @@
 import { useState } from "react";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
 
 const UserInputForm = () => {
-  const [text, setText] = useState('')
-  
-  const handleSubmit = (event) =>{
-    event.preventDefault();
-    console.log({text})
-  }
+  const [text, setText] = useState("");
+  const [mins, setMins] = useState("");
 
-  const handleChange = (event) =>{
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log({ text });
+    console.log({ mins });
+
+    
+  };
+
+  const setTextChange = (event) => {
     setText(event.target.value);
-  }
+  };
+
+  const setMinutesChange = (event) => {
+    setMins(event.target.value);
+  };
+
+  const sliderValue = (value) => {
+    return `${value}`;
+  };
 
   return (
     <div className="userInput">
-    <form onSubmit={handleSubmit}>
-      <label>
-        Location:
-        <input type="text" placeholder="Enter Postcode" value={text} onChange={handleChange} />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+      <form onSubmit={onSubmit}>
+        <label>
+          Enter Your Location:
+          <input
+            type="text"
+            placeholder="Enter Postcode"
+            value={text}
+            onChange={setTextChange}
+          />
+        </label>
+        <Box sx={{ width: 300 }}>
+          <label>
+            How far do you want to walk?
+            <Slider
+              aria-label="How far do you want to walk?"
+              defaultValue={15}
+              getAriaValueText={sliderValue}
+              onChange={setMinutesChange}
+              valueLabelDisplay="auto"
+              step={5}
+              marks={true}
+              min={5}
+              max={30}
+            />
+          </label>
+        </Box>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-
-
-
-
-
-export default UserInputForm
+export default UserInputForm;
