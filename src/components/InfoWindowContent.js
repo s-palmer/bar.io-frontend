c79ae04124446c4271fed0c5e7a7b81761cfdaba
Tim Turvey <tturvey@hotmail.co.uk>
2021-12-08 16:00:55 +0000

    Infowindow content styling css

--- src/components/InfoWindowContent.js
+++ src/components/InfoWindowContent.js
@@ -4,7 +4,7 @@ const InfoWindowContent = ({selectedBar, userLocation}) => {
   const BarLink = `${DIR_URL}${selectedBar.location.lat},${selectedBar.location.lng}&origin=${userLocation.lat},${userLocation.lng}`;
 
   return (
-    <div>
+    <div className="map-infowindow">
       <p>Bar name: {selectedBar.name}</p>
       <a href={BarLink} className="infowindow-button">
         Take me here
