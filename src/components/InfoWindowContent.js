63679fce4b189f1ded1d91bc4169d7e9155efb27
Tim Turvey <tturvey@hotmail.co.uk>
2021-12-09 13:22:13 +0000

    Infowindow image updated to api call

--- src/components/InfoWindowContent.js
+++ src/components/InfoWindowContent.js
@@ -1,7 +1,10 @@
 const DIR_URL = "https://www.google.com/maps/dir/?api=1&destination=";
+const photoApiURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=600";
+const Apikey = process.env.REACT_APP_PLACES_API_KEY;
 
 const InfoWindowContent = ({selectedBar, userLocation}) => {
   const BarLink = `${DIR_URL}${selectedBar.location.lat},${selectedBar.location.lng}&origin=${userLocation.lat},${userLocation.lng}`;
+  const photoURL = `${photoApiURL}&key=${Apikey}&photo_reference=${selectedBar.photo_reference}`;
 
   return (
     <div className="map-infowindow">
@@ -9,7 +12,7 @@ const InfoWindowContent = ({selectedBar, userLocation}) => {
         <img
           width="100%"
           height="auto"
-          src="https://node-test-api-bario.herokuapp.com/images/simmons.jpeg"
+          src={photoURL}
           alt="simmons-bar"
         ></img>
         <a href={BarLink} className="map-infowindow-button">
